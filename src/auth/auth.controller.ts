8e7ba17bf05cc4099f943186a541f5bb073ae0fb
HugoLevick <hlals12346@gmail.com>
2026-03-01 14:46:23 -0600

    add verify endpoint to auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,10 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
+import { GetUser } from './decorators/get-user.decorator';
+import { User } from 'src/users/entities/user.entity';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { Auth } from './decorators/auth.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -15,5 +19,12 @@ export class AuthController {
   @Post('register')
   async register(@Body() registerUserDto: RegisterUserDto) {
     return this.authService.register(registerUserDto);
+  }
+
+  @Auth()
+  @ApiBearerAuth()
+  @Get('verify')
+  async verify(@GetUser() user: User) {
+    return user;
   }
 }
