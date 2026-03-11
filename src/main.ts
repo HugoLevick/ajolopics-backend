6c22887c833891751d8807801dc645d0d22803e0
HugoLevick <hlals12346@gmail.com>
2026-03-10 22:41:26 -0600

    remove global prefix setting from bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,6 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
-
   // Global validation
   app.useGlobalPipes(
     new ValidationPipe({
