563419eeca20d011466b612cffcc66d05b21168e
HugoLevick <hlals12346@gmail.com>
2026-03-10 19:23:59 -0600

    update media variant URLs and keys in migration

---
+++ src/migrations/1773190376795-UpdateAssetUrlsAndKeys.ts
@@ -0,0 +1,28 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+let hostUrl = process.env.HOST || 'http://localhost';
+hostUrl += process.env.PORT ? `:${process.env.PORT}` : '';
+
+export class UpdateAssetUrlsAndKeys1773190376795 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+            UPDATE media_variants mv
+            SET
+                url = CONCAT('${hostUrl}/api/assets/', a.id, '/', mv.variant),
+                key = SUBSTRING(mv.key, 9)
+            FROM assets a
+            WHERE mv."assetId" = a.id;
+        `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+            UPDATE media_variants mv
+            SET
+                url = CONCAT('${hostUrl}/${process.env.UPLOAD_DIR}/', mv.key),
+                key = CONCAT('uploads/', mv.key)
+            FROM assets a
+            WHERE mv."assetId" = a.id;
+        `);
+  }
+}
